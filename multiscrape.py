ad8d02acaca4a6885b6a32a52a293f1ab941af8d
EdJeeOnGitHub <edjee96@gmail.com>
2020-05-29 11:21:32 +0100

    now checks if we've scraped files already

--- multiscrape.py
+++ multiscrape.py
@@ -14,10 +14,14 @@ each Item in the tuple is a string, hence it will need to be enclosed in quotes.
 import os
 import pandas as pd
 import time
-
+import glob
 page_df = pd.read_csv("data/scrape-glassdoor-urls.csv")
+files = glob.glob("data/*[0-9].*csv")
+already_scraped_keys = [int(file.replace("data/", "").replace(".csv", "")) for file in files]
 
-for index, row in page_df.iterrows():
+subset_page_df = page_df[~page_df["gvkey"].isin(already_scraped_keys)]
+
+for index, row in subset_page_df.iterrows():
     command = 'python main.py --headless --credentials data/secret.json --limit 999999 --min_date 2019-09-01 --start_from_url  --url "' + row["webpage_to_scrape_reviews"] + '" -f data/' + str(row["gvkey"]) + '.csv'
 
     print(command)
